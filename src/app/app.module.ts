80fb2ae3ef954115b547dc09c23be89ac6cdaee6
Hafiz Caniago <hafizcode02@gmail.com>
2024-11-11 17:24:26 +0700

    feat: add winston logger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '../user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import * as winston from 'winston';
+import { WinstonModule } from 'nest-winston';
 
 @Module({
   imports: [
+    WinstonModule.forRoot({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.prettyPrint(),
+      ),
+      transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'combined.log' }),
+      ],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
