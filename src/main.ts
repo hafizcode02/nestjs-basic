20268d951f6fe00ab6d8c7be7f4be4ccf8e05902
Hafiz Caniago <hafizcode02@gmail.com>
2024-11-12 11:28:31 +0700

    feat: lifecycle event

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,8 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
 
+  app.enableShutdownHooks();
+
   await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super();
     console.info('PrismaService constructor');
+  }
+  onModuleInit() {
+    console.info('PrismaService onModuleInit: Connect Prisma');
+    this.$connect();
+  }
+  onModuleDestroy() {
+    console.info('PrismaService onModuleDestroy : Disconnect Prisma');
+    this.$disconnect();
   }
 }
